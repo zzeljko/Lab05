ebe2ce14ad4f084680e4e693bd8acdbfef9cbc62
Student User <student@cti.pub.ro>
2018-03-23 13:47:16 +0200

    tasks

--- labtasks/StartedService/StartedService/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedservice/service/StartedService.java
+++ labtasks/StartedService/StartedService/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedservice/service/StartedService.java
@@ -2,6 +2,7 @@ package ro.pub.cs.systems.eim.lab05.startedservice.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -42,6 +43,38 @@ public class StartedService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(Constants.TAG, "onStartCommand() method was invoked");
         // TODO: exercise 5 - implement and start the ProcessingThread
+
+        Thread serviceThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        Intent stringIntent = new Intent();
+                        stringIntent.setAction(Constants.ACTION_STRING);
+                        stringIntent.putExtra(Constants.DATA, Constants.STRING_DATA);
+                        sendBroadcast(stringIntent);
+                        Thread.sleep(Constants.SLEEP_TIME);
+
+                        Intent integerIntent = new Intent();
+                        integerIntent.setAction(Constants.ACTION_INTEGER);
+                        integerIntent.putExtra(Constants.DATA, Constants.INTEGER_DATA + "");
+                        sendBroadcast(integerIntent);
+                        Thread.sleep(Constants.SLEEP_TIME);
+
+                        Intent arrayListIntent = new Intent();
+                        arrayListIntent.setAction(Constants.ACTION_ARRAY_LIST);
+                        arrayListIntent.putExtra(Constants.DATA, Constants.ARRAY_LIST_DATA + "");
+                        sendBroadcast(arrayListIntent);
+                        Thread.sleep(Constants.SLEEP_TIME);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        serviceThread.start();
+        Log.d(Constants.TAG, "Thread.run() was invoked, PID: " + android.os.Process.myPid() + " TID: " + android.os.Process.myTid());
+
         return START_REDELIVER_INTENT;
     }
 
--- labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceBroadcastReceiver.java
+++ labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceBroadcastReceiver.java
@@ -5,11 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.TextView;
 
+import ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants;
+
 public class StartedServiceBroadcastReceiver extends BroadcastReceiver {
 
     private TextView messageTextView;
 
     // TODO: exercise 9 - default constructor
+    public StartedServiceBroadcastReceiver() {
+
+    }
 
     public StartedServiceBroadcastReceiver(TextView messageTextView) {
         this.messageTextView = messageTextView;
@@ -19,8 +24,24 @@ public class StartedServiceBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: exercise 7 - get the action and the extra information from the intent
         // and set the text on the messageTextView
+        String action = intent.getAction();
+        String data = null;
+        if (Constants.ACTION_STRING.equals(action) ||
+                Constants.ACTION_INTEGER.equals(action) ||
+                    Constants.ACTION_ARRAY_LIST.equals(action)) {
+            data = intent.getStringExtra(Constants.DATA);
+        }
+
+        if (messageTextView != null) {
+            messageTextView.setText(messageTextView.getText().toString() + "\n" + data);
+            return;
+        }
 
         // TODO: exercise 9 - restart the activity through an intent
         // if the messageTextView is not available
+        Intent startedServiceActivityIntent = new Intent(context, StartedServiceActivity.class);
+        startedServiceActivityIntent.putExtra(Constants.MESSAGE, data);
+        startedServiceActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        context.startActivity(startedServiceActivityIntent);
     }
 }
